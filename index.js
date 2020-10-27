07198057a2a8f87b11ca5df93dcdfa1000aea00c
NearHuscarl <near.huscarl@gmail.com>
2020-10-27 16:07:37 +0700

    format using prettier

--- index.js
+++ index.js
@@ -1,173 +1,208 @@
-require('dotenv').config({ path: '.env.local' })
+require("dotenv").config({ path: ".env.local" })
 
-const fs = require('fs').promises
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
+const fs = require("fs").promises
+const express = require("express")
+const bodyParser = require("body-parser")
+const fetch = require("node-fetch")
 const jsdom = require("jsdom")
 
 const app = express()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const port = process.env.PORT || 80
-const originWhitelist = process.env.ORIGIN_WHITELIST ? process.env.ORIGIN_WHITELIST.split(',') : []
+const originWhitelist = process.env.ORIGIN_WHITELIST
+  ? process.env.ORIGIN_WHITELIST.split(",")
+  : []
 
 function writeHelper(req, res) {
-    fs.readFile('./README.html', 'utf-8').then(data => {
-        res.writeHead(403, { 'content-type': 'text/html' })
-        res.end(data)
-    })
+  fs.readFile("./README.html", "utf-8").then((data) => {
+    res.writeHead(403, { "content-type": "text/html" })
+    res.end(data)
+  })
 }
 
-app.get('/', (req, res) => {
-    writeHelper(req, res)
+app.get("/", (req, res) => {
+  writeHelper(req, res)
 })
 
 app.use((req, res, next) => {
-    const { origin } = req.headers
-    if (originWhitelist.length > 0 && originWhitelist.indexOf(origin) === -1) {
-        res.writeHead(403, 'Forbidden', req.headers);
-        res.end('The origin "' + origin + '" was not whitelisted by the operator of this proxy.');
-        return
-    }
+  const { origin } = req.headers
+  if (originWhitelist.length > 0 && originWhitelist.indexOf(origin) === -1) {
+    res.writeHead(403, "Forbidden", req.headers)
+    res.end(
+      'The origin "' +
+        origin +
+        '" was not whitelisted by the operator of this proxy.'
+    )
+    return
+  }
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Auth-Cookie");
-    if ('OPTIONS' === req.method) {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Auth-Cookie"
+  )
+  if ("OPTIONS" === req.method) {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
 
 // thank you very much Glorfindel
 // https://meta.stackexchange.com/questions/264678
-app.post('/auth', urlencodedParser, (req, res) => {
-    const { email, password } = req.body
-    const authUrl = 'https://data.stackexchange.com/user/authenticate'
-    const body = new URLSearchParams('oauth2url=https://stackoverflow.com/oauth&openid=')
-    let providenceCookie = ''
+app.post("/auth", urlencodedParser, (req, res) => {
+  const { email, password } = req.body
+  const authUrl = "https://data.stackexchange.com/user/authenticate"
+  const body = new URLSearchParams(
+    "oauth2url=https://stackoverflow.com/oauth&openid="
+  )
+  let providenceCookie = ""
 
-    console.log('POST', authUrl)
-    fetch(authUrl, { method: 'POST', body, redirect: 'manual' }).then(r => {
-        const headers = r.headers.raw()
-        const loginUrl = headers['location'][0]
-        console.log('GET', loginUrl)
-        return fetch(loginUrl, { redirect: 'manual' })
-    }).then(r => {
-        const rawHeaders = r.headers.raw()
-        providenceCookie = rawHeaders['set-cookie'].find(c => c.startsWith('prov='))
-        const loginUrl = rawHeaders['location'][0]
+  console.log("POST", authUrl)
+  fetch(authUrl, { method: "POST", body, redirect: "manual" })
+    .then((r) => {
+      const headers = r.headers.raw()
+      const loginUrl = headers["location"][0]
+      console.log("GET", loginUrl)
+      return fetch(loginUrl, { redirect: "manual" })
+    })
+    .then((r) => {
+      const rawHeaders = r.headers.raw()
+      providenceCookie = rawHeaders["set-cookie"].find((c) =>
+        c.startsWith("prov=")
+      )
+      const loginUrl = rawHeaders["location"][0]
+      const headers = {
+        Cookie: providenceCookie,
+      }
+      console.log("GET", loginUrl)
+
+      if (!providenceCookie) {
+        throw new Error("No providence cookie found.")
+      }
+
+      return fetch(loginUrl, { headers })
+    })
+    .then((r) => r.text())
+    .then((text) => {
+      const dom = new jsdom.JSDOM(text)
+      const { document } = dom.window
+      const actionUrl = document
+        .getElementById("login-form")
+        .getAttribute("action")
+      const headers = { Cookie: providenceCookie }
+      const body = new URLSearchParams({
+        fkey: document.querySelector("input[name='fkey']").value,
+        email,
+        password,
+      })
+      const url = "https://stackoverflow.com" + actionUrl
+      console.log("POST", url)
+
+      return fetch(url, { method: "POST", body, headers, redirect: "manual" })
+    })
+    .then((r) => {
+      return Promise.all([r.headers.raw(), r.text()])
+    })
+    .then(async ([rawHeaders, text]) => {
+      const dom = new jsdom.JSDOM(text)
+      const { document } = dom.window
+      const errorEl = document.querySelector(".js-error-message")
+
+      if (errorEl) {
+        const error = errorEl.textContent.trim()
+        res.send({ error })
+        return
+      }
+
+      if (!rawHeaders["set-cookie"]) {
+        throw new Error("No cookie found.")
+      }
+
+      const accountCookie = rawHeaders["set-cookie"].find((c) =>
+        c.startsWith("acct=")
+      )
+      let redirectUrl = rawHeaders["location"][0]
+
+      if (!accountCookie) {
+        throw new Error("No account cookie found.")
+      }
+
+      // OAuth2 redirection
+      // 1. https://stackoverflow.com/oauth?client_id=...
+      // 2. http://data.stackexchange.com/user/oauth/stackapps?code=...
+      // 3. https://data.stackexchange.com/user/oauth/stackapps?code=...
+      for (let i = 1; i <= 3; i++) {
+        console.log("GET", redirectUrl)
         const headers = {
-            Cookie: providenceCookie,
+          Cookie:
+            i === 1
+              ? `${providenceCookie}; ${accountCookie}`
+              : providenceCookie,
         }
-        console.log('GET', loginUrl)
+        const r = await fetch(redirectUrl, { headers, redirect: "manual" })
+        const rawHeaders = r.headers.raw()
 
-        if (!providenceCookie) {
-            throw new Error("No providence cookie found.")
+        if (i === 3) {
+          const authCookie = rawHeaders["set-cookie"].find((c) =>
+            c.startsWith(".ASPXAUTH=")
+          )
+          res.send({ authCookie })
+        } else {
+          redirectUrl = rawHeaders["location"][0]
         }
-
-        return fetch(loginUrl, { headers })
-    }).then(r => r.text()).then(text => {
-        const dom = new jsdom.JSDOM(text);
-        const { document } = dom.window
-        const actionUrl = document.getElementById('login-form').getAttribute('action')
-        const headers = { Cookie: providenceCookie }
-        const body = new URLSearchParams({
-            fkey: document.querySelector("input[name='fkey']").value,
-            email,
-            password,
-        })
-        const url = 'https://stackoverflow.com' + actionUrl
-        console.log('POST', url)
-
-        return fetch(url, { method: 'POST', body, headers, redirect: 'manual' })
-    }).then(r => {
-        return Promise.all([r.headers.raw(), r.text()]);
-    }).then(async ([rawHeaders, text]) => {
-        const dom = new jsdom.JSDOM(text);
-        const { document } = dom.window
-        const errorEl = document.querySelector('.js-error-message')
-
-        if (errorEl) {
-            const error = errorEl.textContent.trim()
-            res.send({ error })
-            return
-        }
-
-        if (!rawHeaders['set-cookie']) {
-            throw new Error("No cookie found.");
-        }
-
-        const accountCookie = rawHeaders['set-cookie'].find(c => c.startsWith('acct='))
-        let redirectUrl = rawHeaders['location'][0]
-
-        if (!accountCookie) {
-            throw new Error("No account cookie found.");
-        }
-
-        // OAuth2 redirection
-        // 1. https://stackoverflow.com/oauth?client_id=...
-        // 2. http://data.stackexchange.com/user/oauth/stackapps?code=...
-        // 3. https://data.stackexchange.com/user/oauth/stackapps?code=...
-        for (let i = 1; i <= 3; i++) {
-            console.log('GET', redirectUrl)
-            const headers = {
-                Cookie: i === 1 ? `${providenceCookie}; ${accountCookie}` : providenceCookie,
-            }
-            const r = await fetch(redirectUrl, { headers, redirect: 'manual' })
-            const rawHeaders = r.headers.raw()
-
-            if (i === 3) {
-                const authCookie = rawHeaders['set-cookie'].find(c => c.startsWith('.ASPXAUTH='))
-                res.send({ authCookie })
-            } else {
-                redirectUrl = rawHeaders['location'][0]
-            }
-        }
-    }).catch(e => {
-        console.log(e)
-        res.status(500).send()
+      }
+    })
+    .catch((e) => {
+      console.log(e)
+      res.status(500).send()
     })
 })
 
-app.post('/query/run/:siteId/:queryId/:revisionId', urlencodedParser, (req, res) => {
+app.post(
+  "/query/run/:siteId/:queryId/:revisionId",
+  urlencodedParser,
+  (req, res) => {
     // siteId: https://data.stackexchange.com/sites
     const { siteId, queryId, revisionId } = req.params
     const url = `https://data.stackexchange.com/query/run/${siteId}/${queryId}/${revisionId}`
     const body = new URLSearchParams(req.body)
-    const headers = { Cookie: req.headers['auth-cookie'] }
+    const headers = { Cookie: req.headers["auth-cookie"] }
 
     // I'm not a back-end developer (yet?), and it's not the cleanest code I've written but at least it works for now
-    fetch(url, { method: 'POST', body, headers }).then(r => {
+    fetch(url, { method: "POST", body, headers })
+      .then((r) => {
         return r.json()
-    }).then(json => {
+      })
+      .then((json) => {
         const { job_id } = json
         if (!job_id) {
-            res.send(json)
-            return
+          res.send(json)
+          return
         }
 
         // the operation takes some time and result is not in cache. Polling
         console.log(`start polling job: ${job_id}`)
 
         async function poll() {
-            const url = `https://data.stackexchange.com/query/job/${job_id}?=${Date.now()}`
-            console.log('GET', url)
-            const json = await fetch(url)
-                .then(r => r.json())
-            if (json.running) {
-                setTimeout(poll, 1000);
-            } else {
-                res.send(json)
-            }
+          const url = `https://data.stackexchange.com/query/job/${job_id}?=${Date.now()}`
+          console.log("GET", url)
+          const json = await fetch(url).then((r) => r.json())
+          if (json.running) {
+            setTimeout(poll, 1000)
+          } else {
+            res.send(json)
+          }
         }
 
-        setTimeout(poll, 1000);
-    }).catch(e => {
+        setTimeout(poll, 1000)
+      })
+      .catch((e) => {
         console.log(e)
         res.status(500).send()
-    })
-})
+      })
+  }
+)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,12 +1,11 @@
-const md = require('markdown-it')()
-const fs = require('fs').promises
+const md = require("markdown-it")()
+const fs = require("fs").promises
 
 function generateHtmlReadme() {
-	fs.readFile('./README.md', "utf8")
-		.then(data => {
-				const html = md.render(data)
-				fs.writeFile('./README.html', html)
-		})
+  fs.readFile("./README.md", "utf8").then((data) => {
+    const html = md.render(data)
+    fs.writeFile("./README.html", html)
+  })
 }
 
 generateHtmlReadme()
